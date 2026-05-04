a1ee386ac42aade627816c0ec12c371aad48f1b0
Mudit Choudhary <mudit@gmail.com>
2026-05-04 11:01:13 +0530

    by mudit

--- src/apiHandler/urls.js
+++ src/apiHandler/urls.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:5000/api';
+// export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = 'http://192.168.1.41:5000/api';
 
 export const URLS = {
   AUTH: {
